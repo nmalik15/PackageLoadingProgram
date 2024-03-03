179d3b5343f1cce602aabedd59333cafdec148aa
Nenad Malik <nenmalik@amazon.com>
2024-03-03 16:47:21 +0000

    Added ask for number of items, and check if user entered number

--- PLP.py
+++ PLP.py
@@ -6,3 +6,16 @@ unused_capacity = 0
 max_unused_capacity = 0
 max_unused_capacity_package = -1
 
+# 1. Ask user for the number of items
+
+print("\n *** PACKAGE LOADING PROGRAM *** \n")
+
+while True:
+    num_items = input("\nHow many items to pack?: ")
+    if num_items.isdigit(): # checking if user entered number
+        num_items = int(num_items)
+        break
+    else:
+        print("Provide number > 0, not a string! \n")
+        continue
+
